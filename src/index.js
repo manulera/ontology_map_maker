2519b62a4265c541e05b6eadd0c56eeaf38ee162
manulera <manulera14@gmail.com>
2022-07-28 12:25:22 +0100

    removed GO

--- src/index.js
+++ src/index.js
@@ -14,15 +14,15 @@ async function fetchOntologies() {
     throw new Error(`Error! status: ${fypo_resp.status}`);
   }
 
-  const go_resp = await fetch(
-    "http://current.geneontology.org/ontology/go.json"
-  );
-  if (!go_resp.ok) {
-    throw new Error(`Error! status: ${go_resp.status}`);
-  }
+  //   const go_resp = await fetch(
+  //     "http://current.geneontology.org/ontology/go.json"
+  //   );
+  //   if (!go_resp.ok) {
+  //     throw new Error(`Error! status: ${go_resp.status}`);
+  //   }
 
   ONTOLOGY_DICT.FYPO = new OboGraphViz(await fypo_resp.json());
-  ONTOLOGY_DICT.GO = new OboGraphViz(await go_resp.json());
+  //   ONTOLOGY_DICT.GO = new OboGraphViz(await go_resp.json());
 }
 
 function formatPredicate(predicate) {
